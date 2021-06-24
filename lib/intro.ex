eb9de686d5b6660e851228ec1ee8a00c590af353
Zoheb Alli Khan <understandingzak@gmail.com>
2021-06-24 19:54:52 +0530

    Added Files

---
+++ lib/intro.ex
@@ -0,0 +1,19 @@
+defmodule Intro do
+  @moduledoc """
+  Documentation for `Intro`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Intro.hello()
+      :world
+
+  """
+  def hello do
+    :world
+    IO.puts("Hey there")
+  end
+end
